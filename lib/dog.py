92c33c63438cb785e57bdd91de363143ecc74a33
ErnestMaina <you@example.com>
2023-11-27 09:53:19 +0300

    tests passed

--- lib/dog.py
+++ lib/dog.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 class Dog:
-    # Class body goes here
+  # Class body
+  def sit(self):
+    print("The dog is sitting.")
 
-    #Instance method definition
-    pass
+  # Instance Method Definition
+  def bark(self):
+    print("Woof!")
+
+fido = Dog()
+fido .bark()
+
+snoopy = Dog()
+snoopy.bark()
+
+fido = Dog()
+fido .sit()
--- lib/person.py
+++ lib/person.py
@@ -4,4 +4,15 @@ class Person:
     # Class body goes here
 
     #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+        
+    def walk(self):
+        print("The person is walking.")
+    
+fido = Person()
+fido.talk()
+
+fido = Person()
+fido.walk()
+
